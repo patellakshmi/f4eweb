dd009e7c59bc285d7ba6b15bc3b5a91e329dc5df
Lakshmi S. Patel <er.lspatel@gmail.com>
2021-08-15 03:45:00 +0530

    dummy data

--- src/desk/constants/data/DataConstant.js
+++ src/desk/constants/data/DataConstant.js
@@ -1,4 +1,141 @@
+const DATA00 = {
+    img: "BIO1",
+    subTitle:"Physics",
+    subSubTitle:"Base-Part-I",
+    desc:"We belive you are the best",
+    fees:"400 INR",
+    off:"10%",
+    udemyUrl:"http://google.com",
+    youtubeUrl:"http://youtube.com",
+    details:{
+        faculty:"Mr.Anurag Verma",
+        duration:"30 Day, daily 2 hours",
+        mode:"Online",
+        minReq:"Graduation"
+    }
+}
+
+const DATA01 = {
+    img: "BIO1",
+    subTitle:"Physics",
+    subSubTitle:"Base-Part-I",
+    desc:"We belive you are the best",
+    fees:"400 INR",
+    off:"10%",
+    udemyUrl:"http://google.com",
+    youtubeUrl:"http://youtube.com",
+    details:{
+        faculty:"Mr.Anurag Verma",
+        duration:"30 Day, daily 2 hours",
+        mode:"Online",
+        minReq:"Graduation"
+    }
+}
+
+const DATA02 = {
+    img: "BIO1",
+    subTitle:"Physics",
+    subSubTitle:"Base-Part-I",
+    desc:"We belive you are the best",
+    fees:"400 INR",
+    off:"10%",
+    udemyUrl:"http://google.com",
+    youtubeUrl:"http://youtube.com",
+    details:{
+        faculty:"Mr.Anurag Verma",
+        duration:"30 Day, daily 2 hours",
+        mode:"Online",
+        minReq:"Graduation"
+    }
+}
+
+const DATA03 = {
+    img: "BIO1",
+    subTitle:"Physics",
+    subSubTitle:"Base-Part-I",
+    desc:"We belive you are the best",
+    fees:"400 INR",
+    off:"10%",
+    udemyUrl:"http://google.com",
+    youtubeUrl:"http://youtube.com",
+    details:{
+        faculty:"Mr.Anurag Verma",
+        duration:"30 Day, daily 2 hours",
+        mode:"Online",
+        minReq:"Graduation"
+    }
+}
+
+const DATA10 = {
+    img: "BIO1",
+    subTitle:"Physics",
+    subSubTitle:"Base-Part-I",
+    desc:"We belive you are the best",
+    fees:"400 INR",
+    off:"10%",
+    udemyUrl:"http://google.com",
+    youtubeUrl:"http://youtube.com",
+    details:{
+        faculty:"Mr.Anurag Verma",
+        duration:"30 Day, daily 2 hours",
+        mode:"Online",
+        minReq:"Graduation"
+    }
+}
+
+const DATA11 = {
+    img: "BIO1",
+    subTitle:"Physics",
+    subSubTitle:"Base-Part-I",
+    desc:"We belive you are the best",
+    fees:"400 INR",
+    off:"10%",
+    udemyUrl:"http://google.com",
+    youtubeUrl:"http://youtube.com",
+    details:{
+        faculty:"Mr.Anurag Verma",
+        duration:"30 Day, daily 2 hours",
+        mode:"Online",
+        minReq:"Graduation"
+    }
+}
+
+const DATA12 = {
+    img: "BIO1",
+    subTitle:"Physics",
+    subSubTitle:"Base-Part-I",
+    desc:"We belive you are the best",
+    fees:"400 INR",
+    off:"10%",
+    udemyUrl:"http://google.com",
+    youtubeUrl:"http://youtube.com",
+    details:{
+        faculty:"Mr.Anurag Verma",
+        duration:"30 Day, daily 2 hours",
+        mode:"Online",
+        minReq:"Graduation"
+    }
+}
+
+const DATA13 = {
+    img: "BIO1",
+    subTitle:"Physics",
+    subSubTitle:"Base-Part-I",
+    desc:"We belive you are the best",
+    fees:"400 INR",
+    off:"10%",
+    udemyUrl:"http://google.com",
+    youtubeUrl:"http://youtube.com",
+    details:{
+        faculty:"Mr.Anurag Verma",
+        duration:"30 Day, daily 2 hours",
+        mode:"Online",
+        minReq:"Graduation"
+    }
+}
+
+
 export const DATA=[
-                    ["01","02","03","04"],
-                    ["11","12","13","14"]
+                    [DATA00,DATA01,DATA02,DATA03],
+                    [DATA10,DATA11,DATA12,DATA13]
                   ];
