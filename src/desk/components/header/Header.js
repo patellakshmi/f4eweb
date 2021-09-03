b4c8f5a445ece0d69cf86777f52edb2b60b8f89a
Lakshmi S. Patel <er.lspatel@gmail.com>
2021-09-04 03:01:35 +0530

    added login page in desktop and mobile

--- src/desk/components/header/Header.js
+++ src/desk/components/header/Header.js
@@ -3,31 +3,106 @@ import Fight4EduWall from '../../../img/social-media/FacebookWall.png';
 import F4EWithName from '../../../img/logos/F4EWithName.png';
 import RandomTextGenerator from "react-scrolling-text";
 import "./ScrollText.css";
+import "./LoginPage.css";
+import NoticeBoard from "../../../img/NoticeWW.png";
+import Login from "../../../img/home/login.png";
+import Logout from "../../../img/home/logout.png";
+import CloseButton from "../../../img/home/closebutton.png";
+
+import {CENTRAL_CONTENT} from "../../constants/Constants";
+import {Form, Button} from "react-bootstrap";
 
 class Header extends  Component{
+
+    constructor() {
+        super();
+        this.state ={
+            login:false
+        }
+    }
+
+    doLoginLogoutAction=()=>{
+        this.setState({ login: !this.state.login });
+    }
 
     render() {
         return (
             <div>
-                <div style={{ height: 120, width:window.outerWidth, textAlign:"center",
-                    backgroundColor: "#1F384D", backgroundImage: {Fight4EduWall}}} >
-                    <img src={F4EWithName} style={{ height:100, width:210 ,marginLeft:30, marginTop:13}}/>
-                </div>
-                <div style={{height:30, backgroundColor: "#2E86C1", color:"#AAB7B8", fontFamily: "Times New Roman", fontSize:20, textAlign:"center"}}>
-                    <div id="demo-1">
-                        <h3>Education is power to build yourself but the knowledge is material to define yourself - Aditya Patel ( B.Tech - CSE )</h3>
+                {
+                    this.state.login == true &&
+                    <div id="login-page">
+                        <div id="central-login-form"  style={{ top: window.outerHeight/2-175, left: window.outerWidth/2-250}}>
+                            <div style={{float:"right", marginRight:0}} onClick={()=>this.doLoginLogoutAction()}>
+                                <img style={{display: 'block', width: 20, height: 20}}
+                                     className="d-block w-100"
+                                     src={CloseButton}
+                                     alt="Image One"
+                                />
+                            </div>
+                            <div style={{ clear:"both"}}></div>
+                            <div id="login-form-div"  style={{marginLeft:90, marginTop: 15, borderRadius: 12, borderWidth:5, borderColor:"black" ,width:250, height: 200}}>
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Control type="text" placeholder="Enter email or Mobile" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                                        <Form.Control type="password" placeholder="Password" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                        <Form.Check type="checkbox" label="SignUp" />
+                                    </Form.Group>
+                                    <Button variant="primary" type="submit">
+                                        Submit
+                                    </Button>
+                                </Form>
+                            </div>
+                        </div>
                     </div>
+                }
+                {
+                    <div>
+                        <div style={{
+                            height: 120, width: window.outerWidth, textAlign: "center",
+                            backgroundColor: "#1F384D", backgroundImage: {Fight4EduWall}
+                        }}>
+                            <img src={F4EWithName} style={{
+                                height: 100,
+                                width: 210,
+                                marginLeft: (window.outerWidth - 970) / 2,
+                                marginTop: 13
+                            }}/>
+                            <div style={{float: "right", marginTop: 85, marginRight: 3}}>
+                                <img style={{display: 'block', width: 30, height: 30}}
+                                     className="d-block w-100"
+                                     src={this.state.login == true ? Logout : Login}
+                                     alt="Image One"
+                                     onClick={() => this.doLoginLogoutAction()}
+                                />
+                            </div>
+                        </div>
 
-                    {/*<RandomTextGenerator
-                        charList={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']}
-                        text={'Education is power to build yourself but the knowledge is material to define yourself - Aditya Patel ( B.Tech - CSE )'}
-                        interval={30}
-                        timePerChar={30}
-                    />*/}
-                </div>
-                <div style={{ height: 3, marginTop:2, width:window.outerWidth, textAlign:"center",
-                    backgroundColor: "#1F384D", backgroundImage: {Fight4EduWall}}}>
-                </div>
+                        {/* <div style={{ height: 1, marginTop:0, width:window.outerWidth, textAlign:"center",
+                        backgroundColor: "white"}}>
+                    </div>
+                    <div style={{height:30, backgroundColor: "#1F384D", color:"#AAB7B8", fontFamily: "Times New Roman", fontSize:20, textAlign:"center"}}>
+                        <div id="demo-1">
+                            <h3>Education is power to build yourself but the knowledge is material to define yourself - Aditya Patel ( B.Tech - CSE )</h3>
+                        </div>
+
+                        <RandomTextGenerator
+                            charList={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']}
+                            text={'Education is power to build yourself but the knowledge is material to define yourself - Aditya Patel ( B.Tech - CSE )'}
+                            interval={30}
+                            timePerChar={30}
+                        />
+                    </div>*/}
+                        <div style={{
+                            height: 3, marginTop: 2, width: window.outerWidth, textAlign: "center",
+                            backgroundColor: "#1F384D", backgroundImage: {Fight4EduWall}
+                        }}>
+                        </div>
+                    </div>
+                }
             </div>
         )
     }
--- src/mob/components/header/Header.js
+++ src/mob/components/header/Header.js
@@ -1,19 +1,89 @@
-import {Component} from 'react';
+import React, {Component, useEffect} from 'react';
 import Fight4EduWall from '../../../img/social-media/FacebookWall.png';
 import F4EWithName from '../../../img/logos/F4EWithName.png';
+import Logout from "../../../img/home/logout.png";
+import Login from "../../../img/home/login.png";
+import CloseButton from "../../../img/home/closebutton.png";
+
+import {CENTRAL_CONTENT} from "../../constants/Constants";
+import {Form, Button} from "react-bootstrap";
 
 class Header extends  Component{
+
+    constructor() {
+        super();
+        this.state ={
+            login:false
+        }
+    }
+
+    doLoginLogoutAction=()=>{
+        this.setState({ login: !this.state.login });
+
+    }
 
     render() {
         return (
             <div>
-                <div style={{ height: 90, width:window.innerWidth, textAlign:"center",
-                    backgroundColor: "#1F384D", backgroundImage: {Fight4EduWall}}} >
-                    <img src={F4EWithName} style={{ height:60, width:100 ,marginLeft:30, marginTop:13}}/>
-                </div>
-                <div style={{ height: 3, marginTop:2, width:window.innerWidth, textAlign:"center",
-                    backgroundColor: "#1F384D", backgroundImage: {Fight4EduWall}}} >
-                </div>
+                {
+                    this.state.login == true &&
+                    <div id="login-page">
+                        <div id="central-login-form"  style={{ top: window.outerHeight/2-100, left: 15, width: 350, height: 230}}>
+                            <div style={{float:"right", marginRight:0}} onClick={()=>this.doLoginLogoutAction()}>
+                                <img style={{display: 'block', width: 20, height: 20}}
+                                     className="d-block w-100"
+                                     src={CloseButton}
+                                     alt="Image One"
+                                />
+                            </div>
+                            <div style={{ clear:"both"}}></div>
+                            <div id="login-form-div"  style={{marginLeft:40, marginTop: 5, borderRadius: 12, borderWidth:5, borderColor:"black" ,width:250, height: 200}}>
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Control type="text" placeholder="Enter email or Mobile" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                                        <Form.Control type="password" placeholder="Password" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                        <Form.Check type="checkbox" label="SignUp" />
+                                    </Form.Group>
+                                    <Button variant="primary" type="submit">
+                                        Submit
+                                    </Button>
+                                </Form>
+                            </div>
+                        </div>
+                    </div>
+                }
+                {
+                    <div>
+                        <div style={{
+                            height: 90, width: window.outerWidth, textAlign: "center",
+                            backgroundColor: "#1F384D", backgroundImage: {Fight4EduWall}
+                        }}>
+                            <img src={F4EWithName} style={{
+                                height: 60,
+                                width: 100,
+                                marginLeft: (window.outerWidth - 160) / 2,
+                                marginTop: 13
+                            }}/>
+                            <div style={{float: "right", marginTop: 65, marginRight: 10}}>
+                                <img style={{display: 'block', width: 20, height: 20}}
+                                     className="d-block w-100"
+                                     src={this.state.login == true ? Logout : Login}
+                                     alt="Image One"
+                                     onClick={() => this.doLoginLogoutAction()}
+                                />
+                            </div>
+                        </div>
+                        <div style={{
+                            height: 3, marginTop: 2, width: window.outerWidth, textAlign: "center",
+                            backgroundColor: "#1F384D", backgroundImage: {Fight4EduWall}
+                        }}>
+                        </div>
+                    </div>
+                }
             </div>
         )
     }
